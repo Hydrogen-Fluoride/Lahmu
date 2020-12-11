d941793e2d82c3dd954c4f3dd378861223b3308a
Hydrogen-Fluoride <fumi226045@gmail.com>
2020-12-11 18:31:22 +0900

    リセットボタン 濁音等修正

--- main.js
+++ main.js
@@ -6,26 +6,45 @@
     
 // }
 
-// const gameButton = document.getElementById("gameSubmit")
-// gameButton.onclick = function() {
+// const gameButton = document.getElementById("gameSubmit");
+// gameButton.onclick = () => {
 //     const lahmu = document.getElementById("gameLahmu");
 //     const japanese = document.getElementById("gameJapanese");
 //     if (lahmu.value !== "" && japanese.value !== "") {
 //         return;
 //     }
 //     if (japanese.value === "") {
-//         japanese.value = gameLtoJ(lahmu.value)
+//         japanese.value = gameLtoJ(lahmu.value);
 //     } else {
-//         lahmu.value = gameJtoL(japanese.value)
+//         lahmu.value = gameJtoL(japanese.value);
 //     }
 // }
 
-const hiragana = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽぁぃぅぇぉっゃゅょ";
+const hiragana = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+const dakuon = "あいうえおがぎぐげござじずぜぞだぢづでどなにぬねのばびぶべぼまみむめもやゆよらりるれろわをん"
+const handakuon = "あいうえおかきくけこさしすせそたちつてとなにぬねのぱぴぷぺぽまみむめもやゆよらりるれろわをん"
+const small = "ぁぃぅぇぉかきくけこさしすせそたちってとなにぬねのはひふへほまみむめもゃゅょらりるれろわをん"
 
 function animeLtoJ(lahmu) {
     let japanese = "";
     for (let i = 0; i < lahmu.length; i++) {
-        japanese += hiragana.charAt(hiragana.indexOf(lahmu.charAt(i)) - 2);
+        let l = lahmu.charAt(i);
+        let shift = (kind) => {
+            let num = kind.indexOf(l) - 2;
+            if (num < 0) num += kind.length;
+            japanese += kind.charAt(num);
+        }
+        if (hiragana.indexOf(l) !== -1) {
+            shift(hiragana);
+        } else if (dakuon.indexOf(l) !== -1) {
+            shift(dakuon);
+        } else if (handakuon.indexOf(l) !== -1) {
+            shift(handakuon);
+        } else if (small.indexOf(l) !== -1) {
+            shift(small);
+        } else {
+            japanese += " ";
+        }
     }
     return japanese;
 }
@@ -33,13 +52,29 @@ function animeLtoJ(lahmu) {
 function animeJtoL(japanese) {
     let lahmu = "";
     for (let i = 0; i < japanese.length; i++) {
-        lahmu += hiragana.charAt(hiragana.indexOf(japanese.charAt(i)) + 2);
+        let j = japanese.charAt(i);
+        let shift = (kind) => {
+            let num = kind.indexOf(j) + 2;
+            if (num >= kind.length) num -= kind.length;
+            lahmu += kind.charAt(num);
+        }
+        if (hiragana.indexOf(j) !== -1) {
+            shift(hiragana);
+        } else if (dakuon.indexOf(j) !== -1) {
+            shift(dakuon);
+        } else if (handakuon.indexOf(j) !== -1) {
+            shift(handakuon);
+        } else if (small.indexOf(j) !== -1) {
+            shift(small);
+        } else {
+            lahmu += " ";
+        }
     }
     return lahmu;
 }
 
-const animeButton = document.getElementById("animeSubmit")
-animeButton.onclick = function() {
+const animeButton = document.getElementById("animeSubmit");
+animeButton.onclick = () => {
     const lahmu = document.getElementById("animeLahmu");
     const japanese = document.getElementById("animeJapanese");
     if (lahmu.value !== "" && japanese.value !== "") {
@@ -50,4 +85,14 @@ animeButton.onclick = function() {
     } else {
         lahmu.value = animeJtoL(japanese.value);
     }
+}
+
+const animeLahmuResetButton = document.getElementById("animeLahmuReset");
+animeLahmuResetButton.onclick = () => {
+    document.getElementById("animeLahmu").value = "";
+}
+
+const animeJapaneseResetButton = document.getElementById("animeJapaneseReset");
+animeJapaneseResetButton.onclick = () => {
+    document.getElementById("animeJapanese").value = "";
 }
